63b04f93c4dc20111178e9c1ce288f151ad4906b
Delanie Perez <delanieperez11@gmail.com>
2018-05-16 22:48:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-var katzDeli = [];
+var katzDeliLine = [];
 
-function takeANumber(katzDeli, name){
-  katzDeli.push(name);
-  return `Welcome, ${name}. You are number ${katzDeli.length} in line.`;
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
-function nowServing(katzDeli){
-  if(katzDeli.length === 0) {
+function nowServing(katzDeliLine){
+  if(katzDeliLine.length === 0) {
   return "There is nobody waiting to be served!";
   } else {
-    return `Currently serving ${katzDeli.shift()}.`;
+    return `Currently serving ${katzDeliLine.shift()}.`;
   }
 }
 
-function currentLine(katzDeli) {
-  if(katzDeli.length === 0) {
+function currentLine(katzDeliLine) {
+  if(katzDeliLine.length === 0) {
   return "The line is currently empty.";
   } else {
 
-  for (var i = 0; i < katzDeli.length; i++){
+  for (var i = 0; i < katzDeliLine.length; i++){
     var position = i + 1;
     return `The line is currently: + ${position}. + ${name},`;
     }
