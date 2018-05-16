afa3ee62f1793bdd8148807075f6eabbf367df17
Delanie Perez <delanieperez11@gmail.com>
2018-05-16 22:35:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ function currentLine(katzDeli) {
   if(katzDeli.length === 0) {
   return "The line is currently empty.";
   } else {
+    var i = 0;
     for (i = 0; i < katzDeli.length; i++){
       var position = [i + 1];
       var name = katzDeli.length[i];
