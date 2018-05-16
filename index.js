5e79b8fc51408444737d99ae34490d6271cc0252
Delanie Perez <delanieperez11@gmail.com>
2018-05-16 23:15:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function currentLine(katzDeliLine) {
   var deliLine = "The line is currently: ";
   
   for (var i = 0; i < katzDeliLine.length; i++){
-    deliLine = deliLine + `${(i + 1)}. ${katzDeliLine[i]}, `;
+    deliLine += `${(i + 1)}. ${katzDeliLine[i]}, `;
     }
     return deliLine;
   }
