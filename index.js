c58b593ad38d28a375c66ffdcdf102b707ea4d24
Delanie Perez <delanieperez11@gmail.com>
2018-05-16 21:33:38 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+var katzDeli = [];
+
+function takeANumber(katzDeli, name){
+  katzDeli.push(name);
+  return `Welcome, ${name}. You are number ${katzDeli.length} in line.`;
+}
+
+function nowServing(katzDeli){
+  if(katzDeli.length === 0) {
+  return "There is nobody waiting to be served!";
+  } else {
+    
+  }
+}
