fea58160641c0b577ca60635d690ca61f34ea84b
Delanie Perez <delanieperez11@gmail.com>
2018-05-16 21:48:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ function nowServing(katzDeli){
 
 function currentLine(line) {
   if(katzDeli.length === 0) {
-  return "There is nobody waiting to be served!";
-  
+  return "The line is currently empty.";
+  } else {
   for(let i = 0; i < katzDeli.length, i++;){
     return `The line is currently: ${[i] + name}`;
   }
